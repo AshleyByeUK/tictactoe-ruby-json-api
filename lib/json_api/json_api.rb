668963fb40a01097b1958aed3c71bb864e7b0e0b
Ashley Bye <ashley+git@ashleybye.uk>
2018-11-13 15:32:46 +0000

    add json api

--- lib/json_api/json_api.rb
+++ lib/json_api/json_api.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/json'
 require 'sinatra/namespace'
+require 'json_api/game_deserializer'
 require 'json_api/game_serializer'
 require 'json_api/player_deserializer'
 require 'game/game'
@@ -10,26 +11,60 @@ module JsonAPI
     register Sinatra::Namespace
 
     def initialize
+      super
       @player_deserializer = PlayerDeserializer.new
       @game_serializer = GameSerializer.new
+      @game_deserializer = GameDeserializer.new
     end
 
     namespace '/api/v1' do
       new_game = lambda do
         begin
-          players = @player_deserializer.deserialize(request_body["players"])
-          game = Game::Game.new(players)
+          req_body = request_body
+          players = @player_deserializer.deserialize(req_body["players"])
+          game = Game::Game.new(players, board_size: board_size_or(req_body, 3))
           json @game_serializer.serialize(game)
         rescue
           json "error" => "invalid players provided"
         end
       end
 
+      play_game = lambda do
+        begin
+          make_move
+        rescue
+          json "error" => "invalid game or players"
+        end
+      end
+
       post '/game/new', &new_game
+      post '/game/play', &play_game
+    end
+
+    private
+
+    def make_move
+      req_body = request_body
+      current_player = req_body.dig('game', 'current_player') == 1 ? 'player_one' : 'player_two'
+      current_player_type = req_body.dig('players', current_player, 'type')
+      move = req_body.dig('move', 'position')
+      game = @game_deserializer.deserialize(req_body)
+      if move != nil && current_player_type == 'human'
+        game = game.place_token(move) unless game.game_over?
+      elsif current_player_type != 'human'
+        game = game.make_move unless game.game_over?
+      else
+        return json "error" => "move position not provided"
+      end
+      json @game_serializer.serialize(game)
     end
 
     def request_body
       JSON.parse(request.body.read)
+    end
+
+    def board_size_or(req_body, default)
+      req_body.dig('game', 'board_size') == nil ? default : req_body.dig('game', 'board_size')
     end
   end
 end
--- spec/json_api/json_api_spec.rb
+++ spec/json_api/json_api_spec.rb
@@ -7,46 +7,6 @@ require 'json_api/json_api'
 describe JsonAPI do
   include Rack::Test::Methods
 
-  before(:each) do
-    @players = '{
-      "players": {
-        "player_one": {
-          "name": "Player 1",
-          "token": "X",
-          "type": "human"
-        },
-        "player_two": {
-          "name": "Player 2",
-          "token": "O",
-          "type": "human"
-        }
-      }
-    }'
-
-    @serialized_game = '{
-      "game": {
-          "available_positions": [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          "board": [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          "board_size": 3,
-          "current_player": 1,
-          "state": "ready"
-      },
-      "players": {
-          "player_one": {
-              "name": "Player 1",
-              "token": "X",
-              "type": "human"
-          },
-          "player_two": {
-              "name": "Player 2",
-              "token": "O",
-              "type": "human"
-          }
-      }
-  }'
-
-  end
-
   def app
     JsonAPI::JsonAPI.new
   end
@@ -63,12 +23,268 @@ describe JsonAPI do
     end
 
     it 'returns a game when players are specified' do
-      post '/api/v1/game/new', @players, { 'CONTENT_TYPE' => 'application/json' }
+      players = '{
+        "players": {
+          "player_one": {
+            "name": "Player 1",
+            "token": "X",
+            "type": "human"
+          },
+          "player_two": {
+            "name": "Player 2",
+            "token": "O",
+            "type": "human"
+          }
+        }
+      }'
+
+      post '/api/v1/game/new', players, { 'CONTENT_TYPE' => 'application/json' }
 
       expect(last_response).to be_ok
 
       response = JSON.parse(last_response.body)
       expect(response).to have_key('game')
+    end
+
+    it 'creates a game with a board size of 4' do
+      players = '{
+        "game": {
+          "board_size": 4
+        },
+        "players": {
+          "player_one": {
+            "name": "Player 1",
+            "token": "X",
+            "type": "human"
+          },
+          "player_two": {
+            "name": "Player 2",
+            "token": "O",
+            "type": "human"
+          }
+        }
+      }'
+
+      post '/api/v1/game/new', players, { 'CONTENT_TYPE' => 'application/json' }
+
+      expect(last_response).to be_ok
+
+      response = JSON.parse(last_response.body)
+      expect(response).to have_key('game')
+      expect(response['game']).to have_key('board_size')
+      expect(response['game']['board_size']).to eq 4
+    end
+  end
+
+  context 'playing a game' do
+    it 'does not make a human player move when the game is over' do
+      serialized_game = '{
+        "game": {
+          "available_positions": [6, 7, 8, 9],
+          "board": ["X", "X", "X", "O", "O", 6, 7, 8, 9],
+          "board_size": 3,
+          "current_player": 2,
+          "state": "playing",
+          "result": "win",
+          "winner": "Player 1"
+        },
+        "move": {
+          "position": 6
+        },
+        "players": {
+          "player_one": {
+              "name": "Player 1",
+              "token": "X",
+              "type": "human"
+          },
+          "player_two": {
+              "name": "Player 2",
+              "token": "O",
+              "type": "human"
+          }
+        }
+      }'
+
+      post '/api/v1/game/play', serialized_game, { 'CONTENT_TYPE' => 'application/json' }
+
+      expect(last_response).to be_ok
+
+      response = JSON.parse(last_response.body)
+      expect(response['game']['board']).to eq ["X", "X", "X", "O", "O", 6, 7, 8, 9]
+    end
+
+    it 'does not make a computer player move when the game is over' do
+      serialized_game = '{
+        "game": {
+          "available_positions": [6, 7, 8, 9],
+          "board": ["X", "X", "X", "O", "O", 6, 7, 8, 9],
+          "board_size": 3,
+          "current_player": 2,
+          "state": "playing",
+          "result": "win",
+          "winner": "Player 1"
+        },
+        "move": {
+          "position": 6
+        },
+        "players": {
+          "player_one": {
+              "name": "Player 1",
+              "token": "X",
+              "type": "easy"
+          },
+          "player_two": {
+              "name": "Player 2",
+              "token": "O",
+              "type": "easy"
+          }
+        }
+      }'
+
+      post '/api/v1/game/play', serialized_game, { 'CONTENT_TYPE' => 'application/json' }
+
+      expect(last_response).to be_ok
+
+      response = JSON.parse(last_response.body)
+      expect(response['game']['board']).to eq ["X", "X", "X", "O", "O", 6, 7, 8, 9]
+    end
+
+    it 'returns an error when no move is specified for human player' do
+      serialized_game = '{
+        "game": {
+          "available_positions": [6, 7, 8, 9],
+          "board": ["X", "X", "X", "O", "O", 6, 7, 8, 9],
+          "board_size": 3,
+          "current_player": 2,
+          "state": "playing",
+          "result": "win",
+          "winner": "Player 1"
+        },
+        "players": {
+          "player_one": {
+              "name": "Player 1",
+              "token": "X",
+              "type": "human"
+          },
+          "player_two": {
+              "name": "Player 2",
+              "token": "O",
+              "type": "human"
+          }
+        }
+      }'
+
+      post '/api/v1/game/play', serialized_game, { 'CONTENT_TYPE' => 'application/json' }
+
+      expect(last_response).to be_ok
+
+      response = JSON.parse(last_response.body)
+      expect(response).to have_key('error')
+      expect(response['error']).to eq "move position not provided"
+    end
+
+    it 'plays a move when the player is a computer' do
+      serialized_game = '{
+        "game": {
+          "available_positions": [2, 6, 7, 8, 9],
+          "board": ["X", 2, "X", "O", "O", 6, 7, 8, 9],
+          "board_size": 3,
+          "current_player": 1,
+          "state": "playing"
+        },
+        "players": {
+          "player_one": {
+              "name": "Player 1",
+              "token": "X",
+              "type": "easy"
+          },
+          "player_two": {
+              "name": "Player 2",
+              "token": "O",
+              "type": "hard"
+          }
+        }
+      }'
+
+      post '/api/v1/game/play', serialized_game, { 'CONTENT_TYPE' => 'application/json' }
+
+      expect(last_response).to be_ok
+
+      response = JSON.parse(last_response.body)
+      expect(response['game']['board']).not_to eq ["X", 2, "X", "O", "O", 6, 7, 8, 9]
+      expect(response['game']['current_player']).to eq 2
+    end
+
+    it 'returns an error when incorrect game information is provided' do
+      serialized_game = '{
+        "game": {
+          "available_positions": [6, 7, 8, 9],
+          "board_size": 3,
+          "current_player": 2,
+          "state": "playing",
+          "result": "win",
+          "winner": "Player 1"
+        },
+        "move": {
+          "position": 6
+        },
+        "players": {
+          "player_one": {
+              "name": "Player 1",
+              "token": "X",
+              "type": "human"
+          },
+          "player_two": {
+              "name": "Player 2",
+              "token": "O",
+              "type": "human"
+          }
+        }
+      }'
+
+      post '/api/v1/game/play', serialized_game, { 'CONTENT_TYPE' => 'application/json' }
+
+      expect(last_response).to be_ok
+
+      response = JSON.parse(last_response.body)
+      expect(response).to have_key('error')
+      expect(response['error']).to eq "invalid game or players"
+    end
+
+    it 'plays a move when valid position is provided' do
+      serialized_game = '{
+        "game": {
+          "available_positions": [2, 6, 7, 8, 9],
+          "board": ["X", 2, "X", "O", "O", 6, 7, 8, 9],
+          "board_size": 3,
+          "current_player": 1,
+          "state": "playing"
+        },
+        "move": {
+          "position": 2
+        },
+        "players": {
+          "player_one": {
+              "name": "Player 1",
+              "token": "X",
+              "type": "human"
+          },
+          "player_two": {
+              "name": "Player 2",
+              "token": "O",
+              "type": "human"
+          }
+        }
+      }'
+
+      post '/api/v1/game/play', serialized_game, { 'CONTENT_TYPE' => 'application/json' }
+
+      expect(last_response).to be_ok
+
+      response = JSON.parse(last_response.body)
+      expect(response['game']['board']).to eq ["X", "X", "X", "O", "O", 6, 7, 8, 9]
+      expect(response['game']['result']).to eq "win"
+      expect(response['game']['winner']).to eq "Player 1"
     end
   end
 end
